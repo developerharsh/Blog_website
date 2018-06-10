f59ee211f89557c2a9bb60ec56ae4a74c0a49899
developerharsh <harshchauhan880@gmail.com>
2018-06-10 16:09:24 +0530

    new, update, edit routes added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var bodyParser = require("body-parser"),
+methodOverride = require("method-override"),
 mongoose       = require("mongoose"),
 express        = require("express"),
 app            = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost/blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -51,9 +53,52 @@ app.get("/blogs", function(req,res){
     
 });
 
+app.post("/blogs", function(req,res){
+    Blog.create(req.body.blog,function(err, newBlog){
+        if(err)
+        {
+            res.render("new");
+        }
+        else
+        {
+            res.redirect("/blogs");        
+        }
+    });
+    
+});
+
 app.get("/blogs/new", function(req,res){
     res.render("new");
-})
+});
+
+app.get("/blogs/:id", function(req,res){
+    Blog.findById(req.params.id,function(err,foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("show",{blog:foundBlog});
+        }
+    });
+});
+app.get("/blogs/:id/edit",function(req,res){
+    Blog.findById(req.params.id,function(err,foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("edit",{blog:foundBlog});
+        }
+    });
+});
+
+app.put("/blogs/:id", function(req,res){
+    Blog.findOneAndUpdate(req.params.id,req.body.blog,function(err,updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs/"+req.params.id);
+        }
+    });
+});
 
 
 app.listen(2000,function(){
